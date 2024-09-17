fcd771755271062ee4da4fe458601c4891142f7c
pronoy656 <pronoypaul56@gmail.com>
2024-09-17 22:53:03 +0600

    navbar, footer create and signin, signup router setup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <p>welcome home</p>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <p>welcome Home</p>
-      <button className="btn btn-primary text-2xl">Primary</button>
+      <p className="text-3xl font-bold text-center mt-16">welcome home</p>
     </div>
   );
 };
