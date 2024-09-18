65ff59f015cff7ace61c82a3f7f75529aec502d7
mohammad-salim-23 <mohammadsalim017427@gmail.com>
2024-09-18 23:36:19 +0600

    OurStory page basic design complete

--- src/Pages/OurStory/OurStory.jsx
+++ src/Pages/OurStory/OurStory.jsx
@@ -1,7 +1,64 @@
 const OurStory = () => {
   return (
-    <div>
-      <p>OurStory</p>
+    <div style={{ padding: '20px' }}>
+      <h1 className="font-bold text-4xl italic flex justify-center mb-10 font-serif">Our Story</h1>
+      
+      <section className="grid grid-cols-1 md:grid-cols-2">
+      <img
+        src="https://img.freepik.com/free-photo/3d-illustration-closed-black-gift-box_107791-18203.jpg?t=st=1726679783~exp=1726683383~hmac=88b25ef60aad0c890b35790671ebee7f3671d82cb300b2f0a32c4c81cac319d9&w=740"
+        alt="Gift Image"
+        style={{ width: '50%', height: 'auto', marginBottom: '20px' }}
+      />
+        <div>
+        <h2 className="text-xl  uppercase font-opensans ">Our Heritage</h2>
+        <p>
+          We have a rich heritage of delivering unique and memorable gifts to
+          customers all over the world.
+        </p>
+        </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2">
+      <img
+        src="https://img.freepik.com/free-photo/3d-illustration-closed-black-gift-box_107791-18203.jpg?t=st=1726679783~exp=1726683383~hmac=88b25ef60aad0c890b35790671ebee7f3671d82cb300b2f0a32c4c81cac319d9&w=740"
+        alt="Gift Image"
+        style={{ width: '50%', height: 'auto', marginBottom: '20px' }}
+      />
+        <div>
+        <h2 className="text-xl  uppercase font-opensans ">Who We Are</h2>
+        <p>
+          A passionate team dedicated to curating special moments through
+          thoughtful gifts.
+        </p>
+        </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2">
+      <img
+        src="https://img.freepik.com/free-photo/3d-illustration-closed-black-gift-box_107791-18203.jpg?t=st=1726679783~exp=1726683383~hmac=88b25ef60aad0c890b35790671ebee7f3671d82cb300b2f0a32c4c81cac319d9&w=740"
+        alt="Gift Image"
+        style={{ width: '50%', height: 'auto', marginBottom: '20px' }}
+      />
+       <div>
+       <h2 className="text-xl  uppercase font-opensans ">Our Mission</h2>
+        <p>
+          Our mission is to make every occasion memorable with the perfect gift,
+          creating lasting impressions.
+        </p>
+       </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2">
+      <img
+        src="https://img.freepik.com/free-photo/3d-illustration-closed-black-gift-box_107791-18203.jpg?t=st=1726679783~exp=1726683383~hmac=88b25ef60aad0c890b35790671ebee7f3671d82cb300b2f0a32c4c81cac319d9&w=740"
+        alt="Gift Image"
+        style={{ width: '50%', height: 'auto', marginBottom: '20px' }}
+      />
+        <div>
+        <h2 className="text-xl  uppercase font-opensans ">Customer Service</h2>
+        <p>
+          We pride ourselves on delivering exceptional customer service to
+          ensure your gift-giving experience is seamless and joyful.
+        </p>
+        </div>
+      </section>
     </div>
   );
 };
