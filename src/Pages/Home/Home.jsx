22c75d6b363edd97f1c4cdbaaaf1ff752dce3ef2
naiemjoy1 <naiemunhasan@gmail.com>
2024-09-18 10:30:52 +0600

    color and font update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
     <div>
-      <p className="font-bold">welcome home</p>
-      <button className="btn btn-primary">Primary</button>
+      <p className="font-bold font-poppins">welcome home</p>
+      <button className="btn btn-primary font-opensans text-white">
+        Primary
+      </button>
     </div>
   );
 };
