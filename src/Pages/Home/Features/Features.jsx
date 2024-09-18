8c9c01ff2b979ab0269bac051f341572a3521fb9
naiemjoy1 <naiemunhasan@gmail.com>
2024-09-18 20:21:21 +0600

    home section created

---
+++ src/Pages/Home/Features/Features.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FeaturesCard from "./FeaturesCard";
+
+const Features = () => {
+  return (
+    <div>
+      <p>Features</p>
+      <FeaturesCard></FeaturesCard>
+    </div>
+  );
+};
+
+export default Features;
