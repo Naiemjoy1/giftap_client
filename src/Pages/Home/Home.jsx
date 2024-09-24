bd62e014077d7d2d0b2bdc1b82ce760efcec5280
Arif Adnan <arifahamedadnan@gmail.com>
2024-09-24 13:03:44 +0600

    Banner feature added on the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner/Banner";
+import BannerFeature from "./Banner/BannerFeature/BannerFeature";
 import Category from "./Category/Category";
 import Features from "./Features/Features";
 import Gifts from "./Gifts/Gifts";
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div className=" container mx-auto my-10 space-y-5">
       <Banner></Banner>
+      <BannerFeature></BannerFeature>
       <Category></Category>
       <Features></Features>
       <Gifts></Gifts>
