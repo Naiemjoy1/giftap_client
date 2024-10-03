6dd1771f90418c953e550fcc57d759b2f3fca8b6
naiemjoy1 <naiemunhasan@gmail.com>
2024-10-03 21:08:40 +0600

    shop page & details page & cart page design

--- src/Pages/Home/ProductTabs/BestProduct/NewProduct/NewProduct.jsx
+++ src/Pages/Home/ProductTabs/BestProduct/NewProduct/NewProduct.jsx
@@ -19,17 +19,17 @@ const NewProduct = () => {
   }, [axiosPublic]);
 
   return (
-    // this card style will get on ReUseCard Component 
+    // this card style will get on ReUseCard Component
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 p-4">
-        {product.slice(0, 10).map(item => (
+        {product.slice(0, 10).map((item) => (
           <ReUseCard
             key={item._id}
             productId={item._id}
-            productImage={item.image}
+            productImage={item.image.itemImg}
             productPrice={item.price}
             productTitle={item.name}
-            productReview={'1'}
+            productReview={"1"}
           />
         ))}
       </div>
