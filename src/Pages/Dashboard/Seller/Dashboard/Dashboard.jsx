b29822eb44bbb52c7a885cccd062ce6f56386d60
pronoy656 <pronoypaul56@gmail.com>
2024-10-18 00:33:16 +0600

    all title font change

--- src/Pages/Dashboard/Seller/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Seller/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { FaProductHunt } from "react-icons/fa6";
 import { HiMiniCurrencyDollar } from "react-icons/hi2";
+import { MdCancel } from "react-icons/md";
 
 const Dashboard = () => {
   return (
@@ -79,11 +80,11 @@ const Dashboard = () => {
           </div>
           <div className="flex space-x-10 items-center border w-52 h-28 p-3 rounded-xl mt-4 bg-[#E1F7F5]">
             <div>
-              <h1 className="text-base font-semibold">Total product</h1>
-              <h1 className="text-2xl font-bold mt-2">65</h1>
+              <h1 className="text-base font-semibold">Order cancel</h1>
+              <h1 className="text-2xl font-bold mt-2">15</h1>
             </div>
             <div className="text-4xl">
-              <FaProductHunt />
+              <MdCancel />
             </div>
           </div>
         </div>
