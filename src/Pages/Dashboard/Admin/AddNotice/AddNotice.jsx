894c8815e61d84b36a6f3527dbf44e4cb0a958fd
Jeem312 <Shajidajeem312@gmail.com>
2024-10-21 14:23:05 +0600

    changes some design  in noticeboard

--- src/Pages/Dashboard/Admin/AddNotice/AddNotice.jsx
+++ src/Pages/Dashboard/Admin/AddNotice/AddNotice.jsx
@@ -8,9 +8,10 @@ const AddNotice = () => {
   const axiosPublic = useAxiosPublic();
   const [notice, refetch] = useNotice();
   const { user } = useAuth();
- const mynotice = notice?.filter(n=>n.email==user?.email);
- console.log(mynotice)
-  const sortedNotice = mynotice?.sort(
+  
+  const myNotice = notice?.filter(n => n.email === user?.email);
+
+  const sortedNotice = myNotice?.sort(
     (a, b) => new Date(`${b.date} ${b.time}`) - new Date(`${a.date} ${a.time}`)
   );
 
@@ -21,14 +22,14 @@ const AddNotice = () => {
     const date = new Date().toLocaleDateString();
     const time = new Date().toLocaleTimeString();
 
-    const notice = {
+    const newNotice = {
       message: data,
       date: date,
       time: time,
       email: user?.email,
     };
 
-    axiosPublic.post('/notice', notice)
+    axiosPublic.post('/notice', newNotice)
       .then((res) => {
         if (res.data) {
           Swal.fire({
@@ -45,59 +46,56 @@ const AddNotice = () => {
   };
 
   return (
-    <div className="container mx-auto  ">
-    
-      <div className="flex flex-col sm:gap-4 lg:flex-row ">
+    <div className="container mx-auto ">
+      <div className="flex flex-col lg:flex-row ">
         
-      
-        <div className="flex flex-col w-1/2  justify-center items-center ">
+  
+        <div className="w-full lg:w-1/2 mb-8 lg:mb-0 flex flex-col justify-center items-center">
           <div>
             <div className="divider"></div>
-            <h1 className="text-2xl text-primary  ">
-              ------Add Notice-----
-            </h1>
+            <h1 className="text-2xl text-primary text-center">------Add Notice-----</h1>
             <div className="divider"></div>
           </div>
-         <div className=' '>
-         <form onSubmit={handleNotice}>
-            <textarea name='notice' placeholder="Notice" className="textarea textarea-bordered textarea-md w-full max-w-xs mb-4"></textarea>
-            <div>
-              <button className='btn btn-wide bg-primary text-white'>
+          <div>
+            <form onSubmit={handleNotice} className="w-full max-w-xs">
+              <textarea 
+                name='notice' 
+                placeholder="Write your notice here..." 
+                className="textarea textarea-bordered textarea-md w-full mb-4"
+              ></textarea>
+              <button type="submit" className="btn w-full bg-primary text-white ">
                 Add Notice
               </button>
-            </div>
-          </form>
-         </div>
+            </form>
+          </div>
         </div>
 
-       
-        <div className='flex-1 w-1/2 border border-primary rounded-lg'>
+      
+        <div className="w-full lg:w-1/2 border border-primary rounded-lg">
           <div>
             <div className="divider"></div>
-            <h1 className="text-2xl text-primary flex justify-center items-center">
-              ------Your Added Notice-----
-            </h1>
+            <h1 className="text-2xl text-primary flex justify-center items-center">------Your Added Notice-----</h1>
             <div className="divider"></div>
           </div>
 
-         <div className='max-h-[500px] overflow-y-auto'>
-         {sortedNotice.map((n, index) => (
-            <div className="my-6 p-4 border-b border-gray-300" key={n._id}>
-              <h3 className="text-lg font-semibold text-secondary mb-2 flex justify-center items-center underline">
-                Notice: {index + 1}.
-              </h3>
-              <p className="text-gray-800 text-base mb-2">
-                Dear Seller,
-              </p>
-              <p className="text-gray-800 text-base mb-2">
-                {n.message}
-              </p>
-              <p className="text-sm text-gray-500">
-                <span className="font-bold">Published Date & Time:</span> {n.date} & {n.time}
-              </p>
-            </div>
-          ))}
-         </div>
+          <div className="max-h-[500px] overflow-y-auto">
+            {sortedNotice.map((n, index) => (
+              <div className="my-6 p-4 border-b border-gray-300" key={n._id}>
+                <h3 className="text-lg font-semibold text-secondary mb-2 flex justify-center items-center underline">
+                  Notice: {index + 1}.
+                </h3>
+                <p className="text-gray-800 text-base mb-2">
+                  Dear Seller,
+                </p>
+                <p className="text-gray-800 text-base mb-2">
+                  {n.message}
+                </p>
+                <p className="text-sm text-gray-500">
+                  <span className="font-bold">Published Date & Time:</span> {n.date} & {n.time}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
